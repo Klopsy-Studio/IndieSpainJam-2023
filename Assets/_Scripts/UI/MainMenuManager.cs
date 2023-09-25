d97e12a6ced4551a8d48b73f3fc032908ff55698
JUANIG\juanig <darsodios@gmail.com>
2023-09-25 10:58:59 +0200

    Clean up

--- Assets/_Scripts/UI/MainMenuManager.cs
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -43,7 +43,6 @@ public class MainMenuManager : MonoBehaviour
     IEnumerator LoadSceneAsync()
     {
         AsyncOperation op = SceneManager.LoadSceneAsync(sceneToLoad);
-        op.allowSceneActivation = false;
         while (!op.isDone)
         {
 
